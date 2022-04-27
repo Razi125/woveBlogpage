79ad12684685a64b06344e9188a9e2eb73f5f614
muhammad razi <muhammadrazipmdc@gmail.com>
2022-04-28 00:33:43 +0500

    UI FIxing

--- components/woveNews/Details/Details.js
+++ components/woveNews/Details/Details.js
@@ -43,7 +43,80 @@ const Details = () => {
          </div>
 
          {/** para*/}
+         <div>
+            <p>While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.</p>
+            <br/>
+            <br/>
+            <p>While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.</p>
+         </div>
+          
+         {/** Addtional Reading */}
+         <div class='addRead'>
+            <div>
+                <h1>Additional Reading:</h1>
+            </div>
+            <div>
+                <p>For more details on lead generation, read our detailed guide on <span>lead generation</span>, where we dicuss the entire lead gen topic, including outbound strategies and how to qualify leads for nurturing.</p>
+            </div>
+        </div>
+         {/**paraghraph*/}
+        <div class='content'>
+        <div>
+            <p >Many HR tasks are highly regulated, and because of this, even a minor error on the part of a human resources employee could result in considerable legal issues and even financial loss for the company. For example, when resumes are not reviewed in a fair and just manner during the hiring process, a lawsuit may ensue. A HRIS can provide guidance to avoid these types of issues before they escalate.</p>
+            <br/>
+            <br/>
+            <p >When considering HRIS benefits for your organization, the ability to reduce issues and other related errors associated with human oversight or other factors can be considerable. Furthermore, additional HRIS benefits relate to compliance issues. Some so”ware programs are designed to review compliance with specific rules and regulations—this makes it easier to ensure that your company is in compliance with these laws and regulations. Ultimately, this can improve company reputation and help to avoid penalties.</p>
+        </div>
+        </div>
          
+        {/**title*/}
+        <div class='content'>
+        <div>
+            <h1>Reducing Errors and Maintaining Compliance</h1>
+        </div>
+        </div>
+
+        {/** Full Containt */}
+         <div class='content'>
+         {/** ParaGraph*/}
+         <div>
+         <img src='/images/pictwo.png' alt=''/>               
+         </div>
+
+         {/**Title */}
+         <div>
+            <h1>Performing Analyses</h1>
+         </div>
+
+         {/** para*/}
+         <div>
+            <p>Performing analyses and reviewing metrics related to various aspects of the organization can assist with better decision making and also help with spotting patterns. For example, the human resources department is responsible for analyzing hiring costs and calculating the turnover rate in different departments. The results of these calculations may be used to make important business decisions and to develop strategies for moving the organization along a successful path.</p>
+            <br/>
+            <p>HRIS analytical tools give HR employees the ability to perform many pertinent calculations with speed. Employees can collect the data needed within a short period of time and then analyze all of the data in a concise and effective manner. Some software programs are designed to create professional reports on metrics and analysis that can help HR professionals to spot issues at a glance.</p>
+            <br/>
+            <p>Companies can immediately enjoy many HRIS benefits once the human resources information system has been implemented. There are several different types of HRIS systems available for purchase, and each may offer different features and functions. Companies should carefully review the different systems, vendors, and features available in order to find the right program for their needs and budget.</p>
+         </div>
+
+         {/** Social Media */}
+         <div class='content'>
+         <div>
+            <h1>Expand your social media following effectively</h1>
+        </div>
+        </div>
+        <div class='content'>
+        <div>
+            <p>Expanding social media is just a requirement for everything in this day and age. It is because Social media is slowly becoming the new go-to for marketing. However, building a good social media following can be tricky but can serve you with extraordinary outcomes for your business. At this point, approximately 10-20% of a website’s traffic comes from the social media networks alone, translating it to around 300 to 500k page views per month.</p>
+        </div>
+        </div>
+        {/** Addtional Reading */}
+        <div class='addRead'>
+           <div>
+               <h1>Additional Reading:</h1>
+           </div>
+           <div>
+               <p>For more details on lead generation, read our detailed guide on <span>lead generation</span>, where we dicuss the entire lead gen topic, including outbound strategies and how to qualify leads for nurturing.</p>
+           </div>
+       </div>
 
          </div>
         
@@ -127,80 +200,7 @@ const Details = () => {
       </div>
 
       {/** ParhaGraph Detail */}
-      <div className='flex flex-col'>
-       
-       {/** ParaGhraph*/}
-        <div>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir'>While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.</p>
-            <br/>
-            <br/>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir pb-12'>While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.While the HRIS features benefit the organization in many ways, one of the most important of all HRIS benefits relates to the ability of the so”ware program to improve the productivity of human resources employees. These HR systems are highly detailed, and they are designed to enhance and speed up the efforts of HR employees in a number of ways. For example, they can assist with recruitment by simplifying the process of collecting resumes, reviewing candidate information and more.</p>
-        </div>
-
-        {/** Addtional Reading */}
-
-        <div className='bg-[#171717] border-dashed border-2 border-[#0585F2] mb-[60px]'>
-            <div>
-                <h1 className='max-w-[835px] text-[#FFFFFF] text-xl leading-[150%] font-semibold not-italic -tracking-[0.02em] montserrat pl-6 pt-6 pb-4'>Additional Reading:</h1>
-            </div>
-            <div>
-                <p className='max-w-[835px] text-[#D4D4D4] text-base leading-[150%] font-normal not-italic  avenir pl-6 pb-6'>For more details on lead generation, read our detailed guide on <span className='text-[#0585F2] cursor-pointer'>lead generation</span>, where we dicuss the entire lead gen topic, including outbound strategies and how to qualify leads for nurturing.</p>
-            </div>
-        </div>
-
-        {/** para */}
-        <div>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir'>Many HR tasks are highly regulated, and because of this, even a minor error on the part of a human resources employee could result in considerable legal issues and even financial loss for the company. For example, when resumes are not reviewed in a fair and just manner during the hiring process, a lawsuit may ensue. A HRIS can provide guidance to avoid these types of issues before they escalate.</p>
-            <br/>
-            <br/>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir '>When considering HRIS benefits for your organization, the ability to reduce issues and other related errors associated with human oversight or other factors can be considerable. Furthermore, additional HRIS benefits relate to compliance issues. Some so”ware programs are designed to review compliance with specific rules and regulations—this makes it easier to ensure that your company is in compliance with these laws and regulations. Ultimately, this can improve company reputation and help to avoid penalties.</p>
-        </div>
-        {/**Title */}
-        <div>
-            <h1 className='max-w-[883px] text-[#FFFFFF] text-2xl leading-[150%] font-semibold not-italic -tracking-[0.02em] montserrat pt-4 pb-12'>Reducing Errors and Maintaining Compliance</h1>
-        </div>
-        
-        {/**Image For prefernce Anaylices */}
-        <div className='w-full cursor-pointer'>
-         <img className='w-[883px] h-[460px] rounded-lg ' src='/images/pictwo.png' alt=''/>               
-        </div>
-
-        {/**Title */}
-        <div>
-            <h1 className='max-w-[883px] text-[#FFFFFF] text-2xl leading-[150%] font-semibold not-italic -tracking-[0.02em] montserrat pt-12 pb-4'>Performing Analyses</h1>
-        </div>
-        {/** para */}
-        <div>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir'>Performing analyses and reviewing metrics related to various aspects of the organization can assist with better decision making and also help with spotting patterns. For example, the human resources department is responsible for analyzing hiring costs and calculating the turnover rate in different departments. The results of these calculations may be used to make important business decisions and to develop strategies for moving the organization along a successful path.</p>
-            <br/>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir pb-5'>HRIS analytical tools give HR employees the ability to perform many pertinent calculations with speed. Employees can collect the data needed within a short period of time and then analyze all of the data in a concise and effective manner. Some software programs are designed to create professional reports on metrics and analysis that can help HR professionals to spot issues at a glance.</p>
-            <br/>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir pb-5'>Companies can immediately enjoy many HRIS benefits once the human resources information system has been implemented. There are several different types of HRIS systems available for purchase, and each may offer different features and functions. Companies should carefully review the different systems, vendors, and features available in order to find the right program for their needs and budget.</p>
-            <br/>
-            <br/>
-            <p className='max-w-[883px] border-l-2 border-l-[#A3A3A3]  text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir pb-5 pl-6'>“We define human resource information systems (HRIS), how they work, and why they are used in different types of organizations and businesses.”</p>
-        </div>
-        {/** Social Media */}
-        
-         {/**Title */}
-         <div>
-            <h1 className='max-w-[883px] text-[#FFFFFF] text-2xl leading-[150%] font-semibold not-italic -tracking-[0.02em] montserrat pt-12 pb-4'>Expand your social media following effectively</h1>
-        </div>
-        {/** para */}
-        <div>
-            <p className='max-w-[883px] text-[#D4D4D4] text-lg leading-[160%] font-normal not-italic -tracking-[0.01em] avenir pb-12'>Expanding social media is just a requirement for everything in this day and age. It is because Social media is slowly becoming the new go-to for marketing. However, building a good social media following can be tricky but can serve you with extraordinary outcomes for your business. At this point, approximately 10-20% of a website’s traffic comes from the social media networks alone, translating it to around 300 to 500k page views per month.</p>
-        </div>
-
-        {/** Addtional Reading */}
-
-        <div className='bg-[#171717] border-dashed border-2 border-[#0585F2] mb-[60px]'>
-            <div>
-                <h1 className='max-w-[835px] text-[#FFFFFF] text-xl leading-[150%] font-semibold not-italic -tracking-[0.02em] montserrat pl-6 pt-6 pb-4'>Additional Reading:</h1>
-            </div>
-            <div>
-                <p className='max-w-[835px] text-[#D4D4D4] text-base leading-[150%] font-normal not-italic  avenir pl-6 pb-6'>For more details on lead generation, read our detailed guide on <span className='text-[#0585F2] cursor-pointer'>lead generation</span>, where we dicuss the entire lead gen topic, including outbound strategies and how to qualify leads for nurturing.</p>
-            </div>
-        </div>
+      <div className='flex flex-col'> 
         {/** Social Media */}
         <div className='bg-cover bg-center rounded-[6px] bg-bgunsplash bg-no-repeat w-full h-[17rem] mt-16 mb-28'>
             <div>
